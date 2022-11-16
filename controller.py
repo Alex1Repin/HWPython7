995b20ed2803aa7c0f85e7b9a5dd352cbf4a67e8
Alex <al.repin.a@yandex.ru>
2022-11-16 23:17:10 +0300

    add examintion

--- controller.py
+++ controller.py
@@ -1,18 +1,29 @@
 import model
 import model02
 import view
-#import test_int
+import test_keys
 
 def start():
     view.showinfo("Введите данные абонента")
-    #while True:
-    abonent = view.getNumb("input --> ")
-    abon = model02.abonent(abonent)
-    #model.dict_book()
+    while True:
+        abonent = view.getNumb("input --> ")
+        abon = model02.abonent(abonent)
+        if test_keys.test_k(abon, model.dictions1.keys()):
+            result = model.dictions1[abon]
+            view.showinfo(result)
+        elif test_keys.test_k(abon, model.antidict.keys()):
+            result = model.antidict[abon]
+            view.showinfo(result)
+        else:
+            view.showinfo('Нет такого абонента')
+
     
 
-    result = model.dictions1[abon]
+    
 
-    view.showinfo(result)
+    # result = model.antidict[abon]
+    
+    # view.showinfo(result)
+    
 
 
