becf677104cf011f074957f0bd3b14cc243d6925
Alex <al.repin.a@yandex.ru>
2022-11-16 12:51:53 +0300

    add def abonent

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import model
+import model02
 import view
 #import test_int
 
@@ -6,10 +7,12 @@ def start():
     view.showinfo("Введите данные абонента")
     #while True:
     abonent = view.getNumb("input --> ")
-
+    abon = model02.abonent(abonent)
     #model.dict_book()
     
 
-    result = model.dictions1[abonent]
+    result = model.dictions1[abon]
 
     view.showinfo(result)
+
+
