263a869cf64593ffeb6cc1f18fc8fd965c2d3ab1
Alex <al.repin.a@yandex.ru>
2022-11-16 23:33:52 +0300

    add delete while

--- controller.py
+++ controller.py
@@ -5,25 +5,17 @@ import test_keys
 
 def start():
     view.showinfo("Введите данные абонента")
-    while True:
-        abonent = view.getNumb("input --> ")
-        abon = model02.abonent(abonent)
-        if test_keys.test_k(abon, model.dictions1.keys()):
-            result = model.dictions1[abon]
-            view.showinfo(result)
-        elif test_keys.test_k(abon, model.antidict.keys()):
-            result = model.antidict[abon]
-            view.showinfo(result)
-        else:
-            view.showinfo('Нет такого абонента')
+    abonent = view.getNumb("input --> ")
+    abon = model02.abonent(abonent)
+    if test_keys.test_k(abon, model.dictions1.keys()):
+        result = model.dictions1[abon]
+        view.showinfo(result)
+    elif test_keys.test_k(abon, model.antidict.keys()):
+        result = model.antidict[abon]
+        view.showinfo(result)
+    else:
+        view.showinfo('Нет такого абонента')
 
-    
-
-    
-
-    # result = model.antidict[abon]
-    
-    # view.showinfo(result)
-    
+        
 
 
